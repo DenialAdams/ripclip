24016747bded5a298e92c1de35ba4057fc993cd6
Richard McCormack <brick@brick.codes>
2018-09-25 00:05:20 -0400

    fmt

--- build.rs
+++ build.rs
@@ -5,11 +5,10 @@ extern crate winres;
 
 #[cfg(windows)]
 fn main() {
-    let mut res = winres::WindowsResource::new();
-    res.set_icon("clipstack.ico");
-    res.compile().unwrap();
+   let mut res = winres::WindowsResource::new();
+   res.set_icon("clipstack.ico");
+   res.compile().unwrap();
 }
 
 #[cfg(unix)]
-fn main() {
-}
+fn main() {}
